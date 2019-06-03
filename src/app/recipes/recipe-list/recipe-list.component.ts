4b7858b881ac4386a88dea867716bbbbefaddec5
elakioui <zouhaire.elakioui@gmail.com>
2019-06-03 14:36:42 +0000

    updated recipe components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
+
+@Component({
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "recipe 1",
+      "Test recipe",
+      "https://live.staticflickr.com/5836/30219331242_56aa4dfca9_b.jpg"
+    ),
+    new Recipe(
+      "recipe 2",
+      "Test recipe 2",
+      "https://live.staticflickr.com/5836/30219331242_56aa4dfca9_b.jpg"
+    )
+  ];
+  constructor() {}
+
+  ngOnInit() {}
+}
