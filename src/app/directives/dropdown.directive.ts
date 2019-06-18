b94d049a1096e71fe63c12f84aad48772d6baf2f
elakioui <zouhaire.elakioui@gmail.com>
2019-06-18 10:43:24 +0000

    add toggle directive

---
+++ src/app/directives/dropdown.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, HostListener, HostBinding } from "@angular/core";
+
+@Directive({
+  selector: "[appDropdown]"
+})
+export class DropdownDirective {
+  @HostBinding("class.open") isOpen = false;
+  @HostListener("click") toggleOpen() {
+    console.log("DropdownDirective");
+    this.isOpen = !this.isOpen;
+  }
+}
